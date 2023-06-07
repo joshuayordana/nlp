d0cb7dba795dc3dc6f5fc92f2e2bdd822f8f6a87
joshuayordana <joshuayordana8@gmail.com>
2023-06-07 10:08:21 +0700

    upload file + SR

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import streamlit as st
 import os
-from pydub import AudioSegment, silence
+import soundfile as sf
+import matplotlib.pyplot as plt
+import speech_recognition as sr
 
 st.write("""
-# Halo
+# Speech Recognition
 Apa *iya*
 """)
 
 uploded_file = st.file_uploader("Choose a file")
 if uploded_file is not None:
     data, samplerate = sf.read(uploded_file)
-    st.write(samplerate)
+    st.write("Data shape:", len(data))
+    st.write("Sample rate:", samplerate)
+    sr.AudioFile(uploded_file)
+    r = sr.Recognizer()
+    with sr.AudioFile(uploded_file.name) as source:
+        AudioData = r.record(source)
+        alternatives = r.recognize_google(AudioData, show_all=True)
+        st.write(alternatives)
