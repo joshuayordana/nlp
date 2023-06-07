c970dbb4de353933d9c1583f626f5c168781d516
joshuayordana <joshuayordana8@gmail.com>
2023-06-07 09:30:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import speech_recognition as sr
+import os
+import soundfile as sf
 
 st.write("""
 # Halo
 Apa *iya*
 """)
+
+uploded_file = st.file_uploader("Choose a file")
+if uploded_file is not None:
+    data, samplerate = sf.read(uploded_file)
+    st.write(samplerate)
